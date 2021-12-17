14839590f821e4b0cb81939e40257127113ec36c
Dylan Reuter <dylan@debian.lan>
2021-12-17 14:26:20 -0600

    refactor

--- decrypt.go
+++ decrypt.go
@@ -3,56 +3,65 @@ package main
 import (
 	"crypto/aes"
 	"crypto/cipher"
+	"runtime"
 	"fmt"
 	"os"
 )
 
-
+var fileSep string
 
 func main() {
+	home, _ := os.UserHomeDir()
+	fmt.Println("home ", home)
+
+	//testdir := ".\\test"
+	testdir := "./test"
+
+	opSystem := runtime.GOOS
+	fileSep = "/"
+	if opSystem == "windows" {
+		fileSep = "\\"
+	}
+
+	traverseFiles(testdir)
 
 }
 
+func traverseFiles(path string) {
+	var dirs []string
 
-func dirRecurse(dirname os.DirEntry, base string) {
-	files, err := os.ReadDir(base)
+	files, err := os.ReadDir(path)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-	traverseFiles(files, base)
-}
 
-
-func traverseFiles(files []os.DirEntry, base string) {
-	opSystem = runtime.GOOS
-	fileSep := "/"
-	if opSystem == 'windows' {
-		fileSep = "\\"
-	}
 	for _, file := range files {
 		if file.IsDir() {
-			//fmt.Println("traverseFiles read dir path: ", base+"\\"+file.Name())
-			fmt.Println("traverseFiles read dir path: ", base + fileSep + file.Name())
-			base = base + fileSep + file.Name()
-			dirRecurse(file, base)
+			base := path + fileSep + file.Name()
+			dirs = append(dirs, base)
+			fmt.Println("dir path: ", base)
 		} else {
-			fmt.Println("traverseFiles read file path: ", base + fileSep + file.Name())
-			path := base + fileSep + file.Name()
-			data, err := os.ReadFile(path)
-			if err != nil {
-				fmt.Println(err)
-				os.Exit(1)
-			}
-			decryptFile(data, path)
-			fmt.Println("file data: ", data)
+			base := path + fileSep + file.Name()
+			
+			fmt.Println("file to dec: ", base)
+			decryptFile(base)
 		}
+	}
+	for _, dir := range dirs {
+		traverseFiles(dir)
 	}
 }
 
+func decryptFile(path string) {
 
-func decryptFile(data []byte, path string) {
-	key := make([]byte, 32)
+	data, err := os.ReadFile(path)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	key := []byte{}
 
 	c, err := aes.NewCipher(key)
 	if err != nil {
@@ -72,19 +81,16 @@ func decryptFile(data []byte, path string) {
 		os.Exit(1)
 	}
 
-	nonce, ciphertext := data[:nonceSize], data[nonceSize:]
-	plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
+	nonce := data[:nonceSize]
+	encData := data[nonceSize:]
+	decData, err := gcm.Open(nil, nonce, encData, nil)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 
-	fmt.Println("path pre chop ", path)
-	path = path[:len(path)-4]
-	fmt.Println("path post chop ", path)
-	err = os.WriteFile(path, plaintext, 0777)
+	err = os.WriteFile(path, decData, 0777)
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println(string(plaintext))
 }
