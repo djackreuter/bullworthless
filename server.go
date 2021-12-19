31889c4eaab558503e9db7d7989df7c5a77d3884
Dylan Reuter <djreuter45@gmail.com>
2021-12-19 14:10:13 -0600

    refactor

--- server.go
+++ server.go
@@ -11,6 +11,51 @@ import (
 )
 
 func main() {
+
+	privKey, pubKey := genKeys()
+
+	err := writeKeys(privKey, pubKey)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func writeKeys(privKey *rsa.PrivateKey, pubKey *rsa.PublicKey) error {
+	pemPrivFile, err := os.Create("private_key.pem")
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	pemPubFile, err := os.Create("public_key.pem")
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	// start export process
+	var pemPrivBlock = &pem.Block{
+		Type: "RSA PRIVATE KEY",
+		Bytes: x509.MarshalPKCS1PrivateKey(privKey),
+	}
+	var pemPubBlock = &pem.Block{
+		Type: "PUBLIC KEY",
+		Bytes: x509.MarshalPKCS1PublicKey(pubKey),
+	}
+	// write block to file
+	err = pem.Encode(pemPrivFile, pemPrivBlock)
+	if err != nil {
+		return err
+	}
+	pemPrivFile.Close()
+	err = pem.Encode(pemPubFile, pemPubBlock)
+	if err != nil {
+		return err
+	}
+	pemPubFile.Close()
+	return nil
+}
+
+func genKeys() (*rsa.PrivateKey, *rsa.PublicKey) {
 	privKey, err := rsa.GenerateKey(rand.Reader, 2048)
 	if err != nil {
 		fmt.Println(err)
@@ -18,27 +63,7 @@ func main() {
 	}
 
 	pubKey := &privKey.PublicKey
-	fmt.Println("pub key: ", pubKey)
-	fmt.Println("priv key: ", privKey)
-
-	// write to file
-	pemPrivFile, err := os.Create("private_key.pem")
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	// start export process
-	var pemPrivBlock = &pem.Block{
-		Type: "RSA PRIVATE KEY",
-		Bytes: x509.MarshalPKCS1PrivateKey(privKey),
-	}
-	// write block to file
-	err = pem.Encode(pemPrivFile, pemPrivBlock)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	pemPrivFile.Close()
+	return privKey, pubKey
 }
 
 func importKey() {
@@ -52,7 +77,7 @@ func importKey() {
 	pembytes := make([]byte, size)
 
 	buffer := bufio.NewReader(privKeyFile)
-	_, err := buffer.Read(pembytes)
+	_, err = buffer.Read(pembytes)
 
 	data, _ := pem.Decode([]byte(pembytes))
 
