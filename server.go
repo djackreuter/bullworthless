d3b3124e398d3fb22c185f71cb35f08f4b81ce40
Dylan Reuter <djreuter45@gmail.com>
2021-12-21 20:46:46 -0600

    refactor with command line args

--- server.go
+++ server.go
@@ -5,19 +5,38 @@ import (
 	"crypto/rand"
 	"crypto/rsa"
 	"crypto/x509"
+	"flag"
+	"crypto/sha256"
 	"encoding/pem"
+	"encoding/hex"
 	"fmt"
 	"os"
 )
 
 func main() {
 
-	privKey, pubKey := genKeys()
+	var genRsaKeys bool
+	var encAesKey string
 
-	err := writeKeys(privKey, pubKey)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+	flag.BoolVar(&genRsaKeys, "genKeys", false, "Generate server RSA keys")
+	flag.StringVar(&encAesKey, "decryptAES", "", "Decrypt targets AES key")
+	flag.Parse()
+
+	if genRsaKeys == true {
+		err := genKeys()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	}
+
+	if encAesKey != "" {
+		aesKey, err := decryptAesKey(encAesKey)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		fmt.Println(aesKey)
 	}
 }
 
@@ -55,7 +74,7 @@ func writeKeys(privKey *rsa.PrivateKey, pubKey *rsa.PublicKey) error {
 	return nil
 }
 
-func genKeys() (*rsa.PrivateKey, *rsa.PublicKey) {
+func genKeys() error {
 	privKey, err := rsa.GenerateKey(rand.Reader, 2048)
 	if err != nil {
 		fmt.Println(err)
@@ -63,14 +82,20 @@ func genKeys() (*rsa.PrivateKey, *rsa.PublicKey) {
 	}
 
 	pubKey := &privKey.PublicKey
-	return privKey, pubKey
+	return writeKeys(privKey, pubKey)
 }
 
-func importKey() {
+func decryptAesKey(hexKey string) ([]byte, error) {
+	var aesKey []byte
+	aesEncKey, err := hex.DecodeString(hexKey)
+	if err != nil {
+		fmt.Println(err)
+		return aesKey, err
+	}
 	privKeyFile, err := os.Open("private_key.pem")
 	if err != nil {
 		fmt.Println(err)
-		os.Exit(1)
+		return aesKey, err
 	}
 	pemfileinfo, _ := privKeyFile.Stat()
 	var size int64 = pemfileinfo.Size()
@@ -83,14 +108,17 @@ func importKey() {
 
 	privKeyFile.Close()
 
-	privKeyImported, err := x509.ParsePKCS1PrivateKey(data.Bytes)
+	privKey, err := x509.ParsePKCS1PrivateKey(data.Bytes)
 	if err != nil {
 		fmt.Println(err)
-		os.Exit(1)
+		return aesKey, err
 	}
-	fmt.Println("Private Key: ", privKeyImported)
-	pubKey := &privKeyImported.PublicKey
-	fmt.Println("Public Key: ", pubKey)
-	fmt.Println("Private Key: ", privKeyImported)
+	aesKey, err = rsa.DecryptOAEP(sha256.New(), rand.Reader, privKey, aesEncKey, nil)
+	if err != nil {
+		fmt.Println(err)
+		return aesKey, err
+	}
+	fmt.Println(aesKey)
+	return aesKey, nil
 }
 
